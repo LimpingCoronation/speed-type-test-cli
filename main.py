9255f62d1f2760e1d235b495e2a6cbbdfcc67eb8
LimpingCoronation <as.voronin8@gmail.com>
2023-04-29 16:41:47 +0300

    .gitignore is now working

---
+++ main.py
@@ -0,0 +1,18 @@
+from os import system
+from multiprocessing import Process
+
+run = False
+
+
+def start_test():
+    pass
+
+
+def main():
+    print("Hello! This is type speed test.")
+    print("You should type words as possible as faster.")
+    print("Enter s to start or h to help.")
+
+
+if __name__ == '__main__':
+    main()
