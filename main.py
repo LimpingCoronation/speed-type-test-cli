2e727071d6360072d72ab02dce30f84478b49e96
LimpingCoronation <as.voronin8@gmail.com>
2023-04-29 19:24:58 +0300

    Program has necessary functional

--- main.py
+++ main.py
@@ -1,18 +1,71 @@
 from os import system
-from multiprocessing import Process
+from threading import Thread
+from time import sleep
+from random import choice
 
 run = False
+count = 0
+
+def get_words(k):
+    words = []
+    with open('words.txt', 'r') as f:
+        all_words = f.readlines()
+
+        for _ in range(100):
+            word = choice(all_words)
+            words.append(word)
+    return words
 
 
 def start_test():
-    pass
+    global run
+    system('cls')
+    words = get_words(300)
+    run = True
 
+    test = Thread(target=start_typing, args=(words,))
+    test.start()
+
+    sleep(60)
+
+    run = False
+    test.join()
+
+
+def start_typing(words):
+    global count
+    k = 0
+
+    while run:
+        word = words[k]
+        print(word)
+        input_word = input(': ')
+        if word.replace('\n', '') == input_word.strip():
+            count += 1
+        k += 1
+        system('cls')
+    
+    print(f"Your score is {count}\n")
+    count = 0
+
+def log_commands():
+    print("Enter: \n- s to start \n- Ctrl+C to exit\n")
 
 def main():
     print("Hello! This is type speed test.")
-    print("You should type words as possible as faster.")
-    print("Enter s to start or h to help.")
+    print("You should type words as possible as faster.\n")
+    log_commands()
+
+    while True:
+        action = input("Input an action: ")
+
+        if action == 's':
+            start_test()
+            log_commands()
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nProgram was stopped")
